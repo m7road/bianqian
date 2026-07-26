c011110cdd9bf7d5ccdb2ac83767fa3d7debce4c
zn <lnoznx@gmail.com>
2026-07-26 21:03:30 +0800

    📦 PWA 支持：可安装到桌面，离线可用

---
+++ sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'sticky-notes-v1';
+const ASSETS = [
+  '/bianqian/',
+  '/bianqian/index.html',
+  '/bianqian/manifest.json',
+  '/bianqian/icons/icon.svg'
+];
+
+// 安装时缓存核心文件
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+// 激活时清理旧缓存
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// 网络优先，离线时回退缓存
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          if (event.request.method === 'GET') {
+            cache.put(event.request, clone);
+          }
+        });
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
